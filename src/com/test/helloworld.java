cd0eb0bcbac9d63e1033dc7cea948d1b5d1cd5de
SreeViggy <101521745+SreeViggy@users.noreply.github.com>
2022-07-24 22:28:11 +0300

    Hello World

---
+++ src/com/test/helloworld.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class helloworld {
+    public static void main(String[] args) {
+        String string1 = "Vignesh";
+        String string2 = "KR";
+        String strconcat = string1+string2;
+        System.out.println("Iam - "+strconcat);
+    }
+}
