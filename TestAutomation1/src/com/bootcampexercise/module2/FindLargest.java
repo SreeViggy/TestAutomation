52242e1978f846406aa2781a63ea59142f4356ad
SreeViggy <viki.vconnect@gmail.com>
2022-07-25 16:54:23 +0300

    Find Largest - Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module2/FindLargest.java
@@ -0,0 +1,12 @@
+package src.com.bootcampexercise.module2;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        if(x>y)
+            System.out.println(x +" is greateer than "+y);
+        else
+            System.out.println(y +" is greateer than "+x);
+    }
+}
