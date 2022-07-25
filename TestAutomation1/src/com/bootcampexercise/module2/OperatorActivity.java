222b05d7ac8d0d8584b2050a463e88dd5637a836
SreeViggy <viki.vconnect@gmail.com>
2022-07-25 16:43:38 +0300

    Operator Activity - Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module2/OperatorActivity.java
@@ -0,0 +1,11 @@
+package src.com.bootcampexercise.module2;
+
+public class OperatorActivity {
+    public static void main(String[] args) {
+        int var1 = 10;
+        int var2 = 20;
+        int sum = var1 + var2;
+        System.out.println("Sum of "+var1+" and "+var2+" -> "+sum);
+
+    }
+}
