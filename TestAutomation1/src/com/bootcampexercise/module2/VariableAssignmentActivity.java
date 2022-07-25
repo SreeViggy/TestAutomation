7488c46f798d460dfe79bb1bcbc8a8d2175602e3
SreeViggy <viki.vconnect@gmail.com>
2022-07-25 16:35:37 +0300

    Variable Assignment bootcamp - Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module2/VariableAssignmentActivity.java
@@ -0,0 +1,10 @@
+package src.com.bootcampexercise.module2;
+
+public class VariableAssignmentActivity {
+    public static int a = 10;
+    public static void main(String[] args) {
+        System.out.println("Variable before update --> "+a);
+        a = 20;
+        System.out.println("Variable update to --> " + a);
+    }
+}
