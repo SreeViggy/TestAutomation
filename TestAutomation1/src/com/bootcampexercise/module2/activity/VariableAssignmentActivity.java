3e194f042363951bf4ee3c8f20dde31069f834f5
SreeViggy <viki.vconnect@gmail.com>
2022-07-25 15:02:33 +0300

    Testing - New Branch

---
+++ TestAutomation1/src/com/bootcampexercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexercise.module2.activity;
+
+public class VariableAssignmentActivity {
+    public static int a = 10;
+    public static void main(String[] args) {
+        a = 20;
+        System.out.println("Update variable value -- > "+a);
+    }
+
+
+}
---
+++ TestAutomation1/src/com/bootcampexercise/module2/activity/test.java
@@ -0,0 +1,9 @@
+package com.bootcampexercise.module2.activity;
+
+public class test {
+    public static int a = 10;
+    public static void main() {
+        a = 20;
+        System.out.println("Update variable value -- > "+a);
+    }
+}
