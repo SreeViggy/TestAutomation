111250ef4a9fcb0379775af207d570cf46a9fd8a
SreeViggy <viki.vconnect@gmail.com>
2022-07-26 14:20:49 +0300

    Todaysdate - Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module3/activity/DateActivity.java
@@ -0,0 +1,11 @@
+package src.com.bootcampexercise.module3.activity;
+
+import java.util.Date;
+
+public class DateActivity {
+    public static void main (String args[]) {
+        Date todaysDate = new Date();
+        System.out.println("Today's Date "+todaysDate);
+    }
+
+}
