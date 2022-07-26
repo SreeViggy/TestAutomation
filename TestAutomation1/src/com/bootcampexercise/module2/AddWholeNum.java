d2d8dba6e19fa8ffbad4adbd42495e51109472bb
SreeViggy <viki.vconnect@gmail.com>
2022-07-26 09:17:12 +0300

    Whole numbers - Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module2/AddWholeNum.java
@@ -0,0 +1,10 @@
+package src.com.bootcampexercise.module2;
+
+public class AddWholeNum {
+
+    public static void main(String[] args) {
+        for(int x = 50; x <=100; x++) {
+            System.out.println("Whole Numbers is "+x);
+        }
+    }
+}
