002f408de368630e116771bb08544ce55d8d7056
SreeViggy <viki.vconnect@gmail.com>
2022-07-28 09:52:15 +0300

    TryCatchFinallyActivity- Vignesh

---
+++ TestAutomation1/src/com/bootcampexercise/module7/TryCatchFinallyActivity.java
@@ -0,0 +1,26 @@
+package src.com.bootcampexercise.module7;
+
+public class TryCatchFinallyActivity {
+    public static void main(String args[]) {
+        TryCatchFinallyActivity obj = new TryCatchFinallyActivity();
+        obj.catchMeIfYouCan();
+    }
+
+    void catchMeIfYouCan() {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        try {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(arr[i]);
+            }
+        } catch (IndexOutOfBoundsException IndexOutOfBoundsException) {
+            System.out.println(IndexOutOfBoundsException + "is out of bound");
+        } finally {
+            System.out.println("This should get printed even if there is an exception");
+        }
+
+
+    }
+}
+
+
+
